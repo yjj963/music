c5dd5fd5bf6aeef4b0f5ba0fd524f2b8f34320d3
jia <15901226963@163.com>
2019-02-27 20:50:45 +0800

    修改render方法

--- src/js/index/page-1-2.js
+++ src/js/index/page-1-2.js
@@ -1,29 +1,32 @@
 {
     let view={
         el:'#lastestMusic',
+        template:`
+        <li>
+            <h3>{{name}}</h3>
+            <p>
+                <svg class="icon icon-sq">
+                    <use xlink:href="#icon-sq"></use>
+                </svg>
+                {{singer}}
+            </p>
+            <a class='playButton'>
+                <svg class="icon icon-play">
+                    <use xlink:href="#icon-play"></use>
+                </svg>
+            </a>
+        </li>
+        `,
         init(){
             this.$el=$(this.el)
         },
         render(data){
             let songs=data.songs
-            
             songs.map((song)=>{
-                let $li=$(`
-                <li>
-                    <h3>${song.name}</h3>
-                    <p>
-                        
-                        ${song.singer}
-                    </p>
-                    <a>
-                        
-                    </a>
-                </li>
-                `)
-                //$li.append($('.lastestMusicList'))
+                let $li=this.template.replace('{{name}}',song.name)
+                .replace('{{singer}}',song.singer)                
                 this.$el.find('ol.lastestMusicList').append($li)
             })
-            
         }
     }
     let model={
